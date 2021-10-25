35f17e60e4c14893fa195c41acc92a77ac925ad6
RandomGuy090 <randomguy0090@gmail.com>
2021-10-25 14:17:52 +0200

    custom censorship

--- censorship.py
+++ censorship.py
@@ -1,18 +1,59 @@
-from better_profanity import profanity
+
 
 class Censorship(object):
 
+	# def censure_txt(self):
+	# 	if not self.BAD_WORDS:
+	# 		self.BAD_WORDS = ""
+
+	# 	profanity.load_censor_words_from_file(self.BAD_WORDS)
+	# 	self.TEXT = profanity.censor(self.TEXT) 
+	
+	swears_list = None
+
+	def __init__(self, bad_words="", text=""):
+		if not "BAD_WORDS" in globals():
+			self.BAD_WORDS = bad_words
+		if not "TEXT" in globals():
+			self.TEXT = text
+
+		self.load_file(self.BAD_WORDS)
+		
+	def load_file(self, link):
+		with open(link, "r") as f:
+			txt = f.read()
+			self.swears_list = txt.rsplit("\n")
+			
 	def censure_txt(self):
-		if not self.BAD_WORDS:
-			self.BAD_WORDS = ""
+		words = self.TEXT.replace(",", "")
+		words = words.replace(".", "")
+		words = words.replace("\n", " ")
+		words = words.replace("\t", " ")
 
-		profanity.load_censor_words_from_file(self.BAD_WORDS)
-		self.TEXT = profanity.censor(self.TEXT) 
 
+		words = words.split(" ")
 
+		
+
+		for elem in words:
+			elem = elem.replace(" ", "")
+			elem_low = elem.lower()
+
+			for swear in self.swears_list:
+				if swear in elem.lower() and len(swear) > 0:
+				# if swear in elem.lower() and len(swear) > 0:
+					s = elem[0]
+					e = elem[-1]
+
+					elem_rep = s+(len(swear)-2)*"*" + e
+					
+					self.TEXT = self.TEXT.replace(elem, elem_rep)
+		return self.TEXT
 
 
 
 if __name__ == '__main__':
-	Censorship()
+	txt = """TESTING STRING WITH SWEARS	"""
+	print(txt)
+	Censorship(text=txt, bad_words="swears_list.txt")
 
