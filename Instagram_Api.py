41768b6c8f5fd80e732d03d391ef51a5a98b78b4
RandomGuy090 <randomguy0090@gmail.com>
2021-11-21 23:49:32 +0100

    update

--- Instagram_Api.py
+++ Instagram_Api.py
@@ -1,19 +1,34 @@
 from instabot import Bot
 import glob, os
+from config import Config
 
-login = ""
-password = ""
 
-class Instagram_api():
-    def Clear_cookies(self):
+class Instagram_api(Config):
+    ig_bot = None
+
+
+    def login(self):
+        isConfigExists = os.path.isdir("config")
+        if(isConfigExists):
+            self.clear_cookies()
+        self.ig_bot = Bot()
+        # self.ig_bot.login(username=self.LOGIN_INSTAGRAM, password=self.PASSWORD_INSTAGRAM)
+        self.ig_bot.login(username=login, password=password)
+
+
+
+    def clear_cookies(self):
         cookie_del = glob.glob("config/*cookie.json")
         os.remove(cookie_del[0])
 
-    def Upload_post(self, img_path):
-        isConfigExists = os.path.isdir("config")
-        if(isConfigExists):
-            self.Clear_cookies
+
+    def upload_post(self, img_path, caption=""):
         
-        ig_bot = Bot()
-        ig_bot.login(username=login, password=password)
-        ig_bot.upload_photo(img_path, caption="")
+        self.ig_bot.upload_photo(img_path, caption=caption)
+
+
+if __name__  == "__main__":
+
+    insta = Instagram_api()
+    insta.login()
+    insta.upload_post(img_path=path, caption="hello world 2")
