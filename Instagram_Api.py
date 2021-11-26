a859a9c8dea6bc45eea51528b6f285981d5ff1bd
RandomGuy090 <randomguy0090@gmail.com>
2021-11-26 01:19:01 +0100

    code update

--- Instagram_Api.py
+++ Instagram_Api.py
@@ -4,18 +4,18 @@ login = ""
 password = ""
 
 class Instagram_api():
-    def Login(self, login, password):
+    def login(self, login, password):
         bot = Client()
         bot.login(login, password)
         return bot
 
-    def Upload_photo(self, bot, img_path, caption=""):
+    def upload_photo(self, bot, img_path, caption=""):
         bot.photo_upload(
             img_path, 
             caption=caption
         )
 
-    def Upload_album(self, bot, imgs_paths, caption=""):
+    def upload_album(self, bot, imgs_paths, caption=""):
         bot.album_upload(
             imgs_paths,
             caption = caption
