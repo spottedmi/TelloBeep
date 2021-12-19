1550ff434d1c211b26b7c7c9a488f6f005223e91
RandomGuy090 <randomguy0090@gmail.com>
2021-12-19 13:43:16 +0100

    exceptions update

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,16 @@
 
 class Exception_base(Exception):
-	def __init__(self, q_list=None, error=None):
+	def __init__(self, q_list=None, error=None, img=None):
 		self.q_list = None
-		print("Exception")
-		print("token invalid")
+
 		if q_list != None:
 			self.q_list = q_list
 
 		if error != None:
 			self.error = error
+		
+		if img != None:
+			self.img = img
 
 		self.send_exception()
 	
@@ -18,6 +20,9 @@ class Exception_base(Exception):
 			res = {
 				"bot_comment": self.error
 			}
+			if self.img:
+				res["filename"] = self.img
+
 			disc.put(res)
 
 
@@ -38,29 +43,32 @@ class CaptchaRequired(Exception_base):
 	def __init__(self, q_list=None, error=None):
 		super().__init__(q_list=q_list, error="tellonym conneciotn timeout")
 
+class Post_ratio(Exception_base):
+	def __init__(self, q_list=None, error=None, img=None):
+		super().__init__(q_list=q_list, error="post ratio", img=img)
 
 
-class Post_ratio(Exception):
-	q_list = None
-	def __init__(self, q_list=None, error=None):
-		print("Exception")
-		if q_list != None:
-			self.q_list = q_list
+# class Post_ratio(Exception):
+# 	q_list = None
+# 	def __init__(self, q_list=None, error=None):
+# 		print("Exception")
+# 		if q_list != None:
+# 			self.q_list = q_list
 
-		if error != None:
-			self.error = error
+# 		if error != None:
+# 			self.error = error
 		
-		self.send_exception()
+# 		self.send_exception()
 
 	
-	def send_exception(self):
-		if self.q_list != None:
-			disc = self.q_list.get("2main_thread")
-			res = {
-				"bot_comment": self.error
-			}
+# 	def send_exception(self):
+# 		if self.q_list != None:
+# 			disc = self.q_list.get("2main_thread")
+# 			res = {
+# 				"bot_comment": self.error
+# 			}
 
-			disc.put(self.error)
+# 			disc.put(self.error)
 
 
 
