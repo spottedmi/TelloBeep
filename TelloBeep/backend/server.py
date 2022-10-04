0c2869657db907ed9bf3e69a6048595ed6a38915
RandomGuy090 <randomguy0090@gmail.com>
2022-10-04 11:39:05 +0200

    ip address changed

--- TelloBeep/backend/server.py
+++ TelloBeep/backend/server.py
@@ -401,7 +401,7 @@ def json_parser(headers, txt)-> dict:
 
 
 #function executed in thread
-def back_server(q_list, host="127.0.0.1", port=5002):
+def back_server(q_list, host="0.0.0.0", port=5002):
     global queue_list
     queue_list = q_list
 
